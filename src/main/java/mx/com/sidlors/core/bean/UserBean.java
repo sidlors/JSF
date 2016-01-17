43f87af54d03e00f892ba034ddae2b82f96e01b1
Juan Perez Jolote <sidlord@hotmail.com>
2016-01-17 10:07:36 -0600

    Se agrega ejemplo ajax http://localhost:8080/JavaServerFaces/helloAjax.jsf

---
+++ src/main/java/mx/com/sidlors/core/bean/UserBean.java
@@ -0,0 +1,31 @@
+package mx.com.sidlors.core.bean;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class UserBean implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getSayWelcome(){
+    if("".equals(name) || name ==null){
+      return "Null Message";
+    }else{
+      return "Ajax message : Welcome " + name;
+    }
+  }
+  
+}
