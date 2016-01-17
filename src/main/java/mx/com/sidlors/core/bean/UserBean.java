425c71774b2fcf67b077e78c6ede14bffa19caa0
Juan Perez Jolote <sidlord@hotmail.com>
2016-01-17 11:51:21 -0600

    JSF Inject Managed Beans Example

--- src/main/java/mx/com/sidlors/core/bean/UserBean.java
+++ src/main/java/mx/com/sidlors/core/bean/UserBean.java
@@ -1,5 +1,6 @@
 package mx.com.sidlors.core.bean;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
 import java.io.Serializable;
@@ -8,24 +9,32 @@ import java.io.Serializable;
 @SessionScoped
 public class UserBean implements Serializable {
 
-  private static final long serialVersionUID = 1L;
-  
-  private String name;
+	private static final long serialVersionUID = 1L;
 
-  public String getName() {
-    return name;
-  }
+	  @ManagedProperty(value="#{message}")
+	  private MessageBean messageBean;
 
-  public void setName(String name) {
-    this.name = name;
-  }
-  
-  public String getSayWelcome(){
-    if("".equals(name) || name ==null){
-      return "Null Message";
-    }else{
-      return "Ajax message : Welcome " + name;
-    }
-  }
+	  public void setMessageBean(MessageBean messageBean) {
+	    this.messageBean = messageBean;
+	  }
+
+	  private String name;
+
+	  public String getName() {
+	    return name;
+	  }
+
+	  public void setName(String name) {
+	    this.name = name;
+	  }
+
+	  public String getSayWelcome(){
+	    //check if null?
+	    if("".equals(name) || name ==null){
+	      return "";
+	    }else{
+	      return messageBean.getSayWelcome() + name;
+	    }
+	  }
   
 }
